001f80748f98d07cdc667fbb380544f16f135d68
nirasan <niranirao@gmail.com>
2018-03-15 14:14:28 +0900

    add workdir

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v $(pwd)/listings:/listings low-level-programming bash
+docker run -it --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v $(pwd)/listings:/listings -v $(pwd)/workdir:/workdir low-level-programming bash
