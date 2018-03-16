a10639994fc4c7ea22b856b7ca736fef71527609
nirasan <niranirao@gmail.com>
2018-03-16 23:31:12 +0900

    update fizzbuzz

--- workdir/fizzbuzz.asm
+++ workdir/fizzbuzz.asm
@@ -32,49 +32,39 @@ print_buzz:
     ret
 
 print_number:
-    ; get 100
+    ; get 100's place number
     mov rdx, 0
     mov rax, rcx
     mov rbx, 100
     div rbx
-    ; save surplus
+    ; save 10's place number
     push rdx
-    ; print 100
-    lea rsi, [str_numbers + rax]
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, 1
-    ; do
-    push rcx
-    syscall
-    pop rcx
+    call print_rax_number
 
-    ; load surplus
+    ; load 10's place number
     pop rax
-    ; get 10
+    ; get 10's place number
     mov rdx, 0
     mov rbx, 10
     div rbx
     ; save surplus
     push rdx
     ; print 10
-    lea rsi, [str_numbers + rax]
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, 1
-    ; do
-    push rcx
-    syscall
-    pop rcx
+    call print_rax_number
 
     ; load surplus
     pop rax
     ; print 1
+    call print_rax_number
+
+    ret
+
+print_rax_number:
     lea rsi, [str_numbers + rax]
     mov rax, 1
     mov rdi, 1
     mov rdx, 1
-    ; do
+
     push rcx
     syscall
     pop rcx
@@ -93,6 +83,22 @@ print_newline:
 
     ret
 
+check_fizz:
+    mov rdx, 0
+    mov rax, rcx
+    mov rbx, 3
+    div rbx
+    cmp rdx, 0
+    ret
+
+check_buzz:
+    mov rdx, 0
+    mov rax, rcx
+    mov rbx, 5
+    div rbx
+    cmp rdx, 0
+    ret
+
 exit:
     mov rax, 60
     xor rdi, rdi
@@ -103,18 +109,10 @@ _start:
     mov rcx, 1
 
     .loop:
-        mov rdx, 0
-        mov rax, rcx
-        mov rbx, 3
-        div rbx
-        cmp rdx, 0
+        call check_fizz
         je .print_fizz
 
-        mov rdx, 0
-        mov rax, rcx
-        mov rbx, 5
-        div rbx
-        cmp rdx, 0
+        call check_buzz
         je .print_buzz
 
         .print_default:
@@ -125,12 +123,7 @@ _start:
         .print_fizz:
             call print_fizz
 
-            ; check print_buzz
-            mov rdx, 0
-            mov rax, rcx
-            mov rbx, 5
-            div rbx
-            cmp rdx, 0
+            call check_buzz
             je .print_buzz
 
             call print_newline
